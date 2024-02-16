c301e39fc06581728f354a3a11ebc631d811c9e1
Васильев Иван <a3534@yandex.ru>
2024-02-16 11:38:59 +0300

    add paging

---
+++ RatesApplication/Components/Pages/Calc.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace RatesApplication.Components.Pages
+{
+    public partial class Calc
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ RatesApplication/Components/Pages/Rates.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using RatesServices;
+
+namespace RatesApplication.Components.Pages
+{
+    public partial class Rates
+    {
+        [Inject]
+        IRatesQueryService RatesQueryService { get; set; } = default!;
+
+        private RatesModels.RateListItemDto[]? rates;
+
+        protected override async Task OnInitializedAsync()
+        {
+   
+            rates = await RatesQueryService.GetRatesAsync(30).ToArrayAsync();
+
+        }
+
+        private async Task PageSelected(int page)
+        {
+
+            rates = await RatesQueryService.GetRatesAsync(30, (page-1)* 30).ToArrayAsync();
+
+        }
+
+
+    }
+}
