232b377a8848611ab0db93c29b822449ae43ea4b
Васильев Иван <a3534@yandex.ru>
2024-03-12 00:12:41 +0300

    z

--- Vasiliev.Idp.Orchestrator/Services/IKafkaProducerService.cs
+++ Vasiliev.Idp.Orchestrator/Services/IKafkaProducerService.cs
@@ -5,6 +5,5 @@ namespace Vasiliev.Idp.Orchestrator.Services;
 public interface IKafkaProducerService
 {
     void SendRates(IEnumerable<RateDataDto> rate, CancellationToken ct);
-
     void SendCommand(RateCommandDto command, CancellationToken ct);
 }
